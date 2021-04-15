6ae2f0dc5504dae44930c9234d89d5a419d95b7f
Omar Abuhassan <39484358+OmarAbuhassan@users.noreply.github.com>
2021-04-15 17:05:43 +0300

    submited

--- PA2.py
+++ PA2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """ 
-PA1.py
+PA2.py
 
 Requirements:
     Python 3.7.3 and up
@@ -80,18 +80,8 @@ IMAGE_PATH = 'Images/60'  # path to the chess pieces
 
 
 BLANK = 0  # piece names
-PAWNB = 1
-KNIGHTB = 2
-BISHOPB = 3
-ROOKB = 4
-KINGB = 5
 QUEENB = 6
-PAWNW = 7
-KNIGHTW = 8
-BISHOPW = 9
-ROOKW = 10
-KINGW = 11
-QUEENW = 12
+
 
 
 # Absolute rank based on real chess board, white at bottom, black at the top.
@@ -107,68 +97,18 @@ RANK_1 = 0
 
 
 
-white_init_promote_board = [[QUEENW, ROOKW, BISHOPW, KNIGHTW]]
+white_init_promote_board = [[]]
 
-black_init_promote_board = [[QUEENB, ROOKB, BISHOPB, KNIGHTB]]
-
-
-HELP_MSG = """(A) To play a game
-You should be in Play mode.
-1. Mode->Play
-2. Make move on the board
-
-(B) To play as black
-You should be in Neutral mode
-1. Board->Flip
-2. Mode->Play
-3. Engine->Go
-If you are already in Play mode, go back to 
-Neutral mode via Mode->Neutral
-
-(C) To flip board
-You should be in Neutral mode
-1. Board->Flip
-  
-(D) To paste FEN
-You should be in Play mode
-1. Mode->Play
-2. FEN->Paste
-
-(E) To show engine search info after the move                
-1. Right-click on the Opponent Search Info and press Show
-
-(F) To Show book 1 and 2
-1. Right-click on Book 1 or 2 press Show
-"""
+black_init_promote_board = [[QUEENB]]
 
 
-# Images/60
 blank = os.path.join(IMAGE_PATH, 'blank.png')
-bishopB = os.path.join(IMAGE_PATH, 'bB.png')
-bishopW = os.path.join(IMAGE_PATH, 'wB.png')
-pawnB = os.path.join(IMAGE_PATH, 'bP.png')
-pawnW = os.path.join(IMAGE_PATH, 'wP.png')
-knightB = os.path.join(IMAGE_PATH, 'bN.png')
-knightW = os.path.join(IMAGE_PATH, 'wN.png')
-rookB = os.path.join(IMAGE_PATH, 'bR.png')
-rookW = os.path.join(IMAGE_PATH, 'wR.png')
 queenB = os.path.join(IMAGE_PATH, 'bQ.png')
-queenW = os.path.join(IMAGE_PATH, 'wQ.png')
-kingB = os.path.join(IMAGE_PATH, 'bK.png')
-kingW = os.path.join(IMAGE_PATH, 'wK.png')
+
+images = {QUEENB: queenB, BLANK: blank}
 
 
-images = {BISHOPB: bishopB, BISHOPW: bishopW, PAWNB: pawnB, PAWNW: pawnW,
-          KNIGHTB: knightB, KNIGHTW: knightW,
-          ROOKB: rookB, ROOKW: rookW, KINGB: kingB, KINGW: kingW,
-          QUEENB: queenB, QUEENW: queenW, BLANK: blank}
-
-
-# Promote piece from psg (pysimplegui) to pyc (python-chess)
-promote_psg_to_pyc = {KNIGHTB: chess.KNIGHT, BISHOPB: chess.BISHOP,
-                      ROOKB: chess.ROOK, QUEENB: chess.QUEEN,
-                      KNIGHTW: chess.KNIGHT, BISHOPW: chess.BISHOP,
-                      ROOKW: chess.ROOK, QUEENW: chess.QUEEN,}
+promote_psg_to_pyc = {QUEENB: chess.QUEEN}
 
 
 INIT_PGN_TAG = {
@@ -178,27 +118,7 @@ INIT_PGN_TAG = {
 }
 
 
-# (1) Mode: Neutral
-menu_def_neutral = [
-        ['&Mode', ['Genetic Run',"A* Run"]],
-
-]
-
-# (2) Mode: Play, info: hide
-menu_def_play = [
-        ['&Mode', ['Neutral']],
-        ['&Game', ['&New::new_game_k',
-                   'Save to My Games::save_game_k',
-                   'Save to White Repertoire',
-                   'Save to Black Repertoire',
-                   'Resign::resign_game_k',
-                   'User Wins::user_wins_k',
-                   'User Draws::user_draws_k']],
-        ['FEN', ['Paste']],
-        ['&Engine', ['Go', 'Move Now']],
-        ['&Help', ['About']],
-]
-
+menu_def_neutral = [['&Mode', ['Backtracking']]]
 
 
 def initial_board (self,window):
@@ -219,8 +139,7 @@ def initial_board (self,window):
              [0, 0, 0, 0, 0, 0, 0, 0],
              [0, 0, 0, 0, 0, 0, 0, 0]
              ]
-    # solveNQUtil(board, 0)
-    # printSolution(board)
+
     fitnessNum = 1
     list=[]
     global best
@@ -234,16 +153,8 @@ def initial_board (self,window):
         if fitnessNum < best_atack:
             best_atack = fitnessNum
             best = list
-        print(fitnessNum)
-        print(list)
         savedSteps.append(list)
-# time.sleep(2)
 
-
-    print(steps)
-
-
-    print("====")
 
     return best
 
@@ -295,16 +206,8 @@ class EasyChessGui:
 
 
     def update_game(self, mc, user_move, time_left, user_comment):
-        """
-        Used for saving moves in the game.
 
-        :param mc: move count
-        :param user_move:
-        :param time_left:
-        :param user_comment: Can be a 'book' from the engine
-        :return:
-        """
-        # Save user comment
+
         if self.is_save_user_comment:
             # If comment is empty
             if not (user_comment and user_comment.strip()):
@@ -398,55 +301,18 @@ class EasyChessGui:
 
 
     def update_labels_and_game_tags(self, window, human='Human'):
-        """ Update player names """
         engine_id = self.opp_id_name
-        # if self.is_user_white:
-        #     window.FindElement('_White_').Update(human)
-        #     window.FindElement('_Black_').Update(engine_id)
-
-        # else:
-        #     window.FindElement('_White_').Update(engine_id)
-        #     window.FindElement('_Black_').Update(human)
-        #     self.game.headers['White'] = engine_id
-        #     self.game.headers['Black'] = human
-
-
 
     def relative_row(self, s, stm):
-        """
-        The board can be viewed, as white at the bottom and black at the
-        top. If stm is white the row 0 is at the bottom. If stm is black
-        row 0 is at the top.
-        :param s: square
-        :param stm: side to move
-        :return: relative row
-        """
         return 7 - self.get_row(s) if stm else self.get_row(s)
 
     def get_row(self, s):
-        """
-        This row is based on PySimpleGUI square mapping that is 0 at the
-        top and 7 at the bottom.
-        In contrast Python-chess square mapping is 0 at the bottom and 7
-        at the top. chess.square_rank() is a method from Python-chess that
-        returns row given square s.
-
-        :param s: square
-        :return: row
-        """
         return 7 - chess.square_rank(s)
 
     def get_col(self, s):
-        """ Returns col given square s """
         return chess.square_file(s)
 
     def redraw_board(self, window):
-        """
-        Redraw board at start and afte a move.
-
-        :param window:
-        :return:
-        """
         for i in range(8):
             for j in range(8):
                 color = self.sq_dark_color if (i + j) % 2 else \
@@ -468,17 +334,8 @@ class EasyChessGui:
 
 
 
-
-
-
     def create_board(self, is_user_white=True,list=[]):
-        """
-        Returns board layout based on color of user. If user is white,
-        the white pieces will be at the bottom, otherwise at the top.
 
-        :param is_user_white: user has handling the white pieces
-        :return: board layout
-        """
         board = [[0, 0, 0, 0, 0, 0, 0, 0],
                  [0, 0, 0, 0, 0, 0, 0, 0],
                  [0, 0, 0, 0, 0, 0, 0, 0],
@@ -495,7 +352,6 @@ class EasyChessGui:
         board_layout = []
 
         if is_user_white:
-            # Save the board with black at the top
             start = 0
             end = 8
             step = 1
@@ -504,37 +360,32 @@ class EasyChessGui:
             end = -1
             step = -1
 
-        # Loop through the board and create buttons with images
         for i in range(start, end, step):
-            # Row numbers at left of board is blank
             row = []
             for j in range(start, end, step):
                 piece_image = images[self.psg_board[i][j]]
                 row.append(self.render_square(piece_image, key=(i, j), location=(i, j)))
             board_layout.append(row)
-
         return board_layout
 
-    def build_main_layout(self, is_user_white=True,name="gfhc",list=[0,0,0,0,0,0,0,0]):
-
+    def build_main_layout(self, is_user_white=True,name="gfhc",list=[0, 0, 0, 0, 0, 0, 0, 0]):
         sg.ChangeLookAndFeel(self.gui_theme)
         sg.SetOptions(margins=(0, 3), border_width=1)
         global savedIndex
         global savedSteps
-        print(savedSteps)
-        print("******")
+
 
         # Define board
-        board_layout = self.create_board(is_user_white,list)
+        board_layout = self.create_board(is_user_white, list)
         board_controls = [
-            [sg.Button("previous", key="previous"),sg.Button("next", key="next")],
-            [sg.Text('generations'),sg.InputText(generations),],
-            [sg.Text('steps:'), sg.InputText(str(savedIndex) +" / "+str(len(savedSteps)) ,key="index"), ],
-
+            [sg.Button("Start", key="Start")],
+            [sg.Text("you can run pure backtracking")],
+            [sg.Radio('pure', 1, key='pure', default=True), sg.Radio('MRV', 1, key='MRV'),
+             sg.Radio('LCV', 1, key='LCV')],
 
         ]
 
-        board_tab = [[sg.Column(board_layout,key="board"),sg.Column(board_controls)]]
+        board_tab = [[sg.Column(board_layout, key="board"), sg.Column(board_controls)]]
 
         self.menu_elem = sg.Menu(menu_def_neutral, tearoff=False)
 
@@ -545,47 +396,42 @@ class EasyChessGui:
         ]
 
         return layout
-    
+
 
 
     def main_loop(self):
-
         layout = self.build_main_layout(True)
-
         # Use white layout as default window
         window = sg.Window('{} {}'.format(APP_NAME, APP_VERSION),
                            layout, default_button_element_size=(12, 1),
                            auto_size_buttons=False,
                            icon=ico_path[platform]['pecg'])
-
-
-
         self.init_game()
-
+        savedIndex = len(savedSteps)- 1
         while True:
             self.update_labels_and_game_tags(window, human=self.username)
             break
 
         # Mode: Neutral, main loop starts here
         while True:
-            button, value = window.Read(timeout=50)
+            button, values= window.Read(timeout=50)
             global generations
-            try:
-                generations = int(list(value.values())[1])
-                temp = str(savedIndex + 1) + " out of " + str(len(savedSteps))
-                window.FindElement("index").update(temp)
 
-            except:
-                m =1;
-            # print(generations)
-            # generations = int(value)
-            # Mode: Neutral
             if button == "previous":
 
                 if(savedIndex > 0):
                     savedIndex = savedIndex - 1
+                    print(savedSteps[savedIndex])
                     window.FindElement("board").update(self.create_board(self, savedSteps[savedIndex]))
                     self.redraw_board(window)
+
+            if button == "Start":
+
+                initial_board(self, window)
+
+                vector = backtracking(MRV=values['MRV'], LCV=values['LCV'])
+                window.FindElement("board").update(self.create_board(self, vector))
+                self.redraw_board(window)
 
             if button == "next":
                 last_index = len(savedSteps)
@@ -593,73 +439,22 @@ class EasyChessGui:
                     savedIndex = savedIndex + 1
                     last_index = len(savedSteps) - savedIndex
                     window.FindElement("board").update(self.create_board(self, savedSteps[last_index]))
-
-
-
                     self.redraw_board(window)
+
             if button is None:
                 logging.info('Quit app from main loop, X is pressed.')
                 break
 
-            if button == 'Genetic Run':
-
-                    global best_atack
-                    best_atack = 10
-                    # run(layout)
-                    savedSteps.clear()
-                    initial_board(self,window)
-                    # print(len(savedSteps))
-                    savedIndex = len(savedSteps) - 1
-                    savedSteps.sort(key=fitness, reverse = True)
-                    window.FindElement("board").update(self.create_board(self, savedSteps[savedIndex]))
-
-
-                    self.redraw_board(window)
-
-            if button == 'A* Run':
-
-                    initial_board(self, window)
-                    window.FindElement("board").update(self.create_board(True, backtracking()))
-                    self.redraw_board(window)
-                    # best_atack = 10
-                    # # run(layout)
-                    # savedSteps.clear()
-                    # initial_board(self,window)
-                    # print(len(savedSteps))
-                    # savedIndex = len(savedSteps) - 1
-                    # savedSteps.sort(key=fitness, reverse = True)
-                    # window.FindElement("board").update(self.create_board(self, savedSteps[savedIndex]))
-                    #
-                    #
-                    # self.redraw_board(window)
-
-
-
-
-
-# def run(hi):
-
-        # time.sleep(5)
-
 
 N = 8
 
-""" ld is an array where its indices indicate row-col+N-1  
-(N-1) is for shifting the difference to store negative  
-indices """
 ld = [0] * 30
 
-""" rd is an array where its indices indicate row+col  
-and used to check whether a queen can be placed on  
-right diagonal or not"""
 rd = [0] * 30
 
-"""column array where its indices indicates column and  
-used to check whether a queen can be placed in that  
-    row or not"""
 cl = [0] * 30
 
-""" A utility function to print solution """
+
 
 def fitness (chromosome):
 
@@ -690,58 +485,13 @@ def fitness (chromosome):
     return fitness_value
 
 
-
-def printSolution(board):
-    for i in range(N):
-        for j in range(N):
-            print(board[i][j], end = " ")
-        print()
-    print(steps)
-
-""" A recursive utility function to solve N  
-Queen problem """
-
 steps=0
 best=[]
 generations = 500
 best_atack=10
 savedSteps=[]
 savedIndex=0
-def solveNQUtil(board, col):
-    """ base case: If all queens are placed
-        then return True """
-    if (col >= N):
-        print("solved")
-        return True
-    global steps
 
-    steps=0
-    for i in range(N):
-        steps = steps+1
-
-        if ((ld[i - col + N - 1] != 1 and
-             rd[i + col] != 1) and cl[i] != 1):
-
-            board[i][col] = QUEENB
-            ld[i - col + N - 1] = rd[i + col] = cl[i] = 1
-
-            if (solveNQUtil(board, col + 1)):
-                return True
-
-            board[i][col] = 0  # BACKTRACK
-            ld[i - col + N - 1] = rd[i + col] = cl[i] = 0
-
-    return False
-
-
-""" This function solves the N Queen problem using  
-Backtracking. It mainly uses solveNQUtil() to  
-solve the problem. It returns False if queens  
-cannot be placed, otherwise, return True and  
-prints placement of queens in the form of 1s.  
-Please note that there may be more than one  
-solutions, this function prints one of the  
-feasible solutions."""
 
 def is_solution (vector):
     length = len(vector)
@@ -780,15 +530,11 @@ def is_p_and_L(vector):
 def is_legal(x, vector):
     length = len(vector)
     for i in range(0, length):
-        # print("isL i ",i)
         if x != i and vector[i] != 0:
-            # print("isL i ", i)
             if vector[i] == vector[x]:
-
                 return False
             elif vector[i] - vector[x] == (i - x) or vector[i] - vector[x] == (x - i):
                 return False
-
     return True
 
 
@@ -796,7 +542,6 @@ def lcv(x, vector, stack):
     length = len(vector)
     myValue = 0
     lest = 0
-
     for i in range(1, length+1):
         temp = 0
         vector[x] = i
@@ -828,109 +573,53 @@ def mrv(vector):
     for i in range(0, length):
         temp = 0
         if vector[i] == 0:
-            # print("i is", i)
             for j in range(1, length+1):
                 vector[i] = j
                 if is_legal(i, vector):
                     temp = temp + 1
-                    # print("temp", temp)
             vector[i] = 0
-
             if min > temp:
                 min = temp
                 myk = i
-                # print("k is", myk, "\n")
-
-    # print(vector)
     return myk
 
 
-def backtracking():
-    noSolution = False;
+
+def backtracking (MRV= False, LCV=False):
     vector = [0, 0, 0, 0, 0, 0, 0, 0]
-    k = 0
-    while k >= 0 and noSolution == False:
-        while vector[k] <= 7:
-            if True:
-                print("k", k)
-                temp = lcv(k, vector)
-                if temp != 0:
-                    vector[k] = temp
-                else:
-                    noSolution= True
-                    break
-                print(vector)
-            else:
-                vector[k] = vector[k]+1
-            if is_solution(vector):
-                print("done")
-                print(vector)
-                for i in range(0, len(vector)):
-                    vector[i] -= 1
-                print(vector)
-                return vector
-            elif is_p_and_L(vector):
-                k += 1
-        vector[k] = 0
-        k -= 1
-    if noSolution:
-        return [0,0]
-
-
-
-def backtraking_with_mrv (MRV,LCV):
-    vector = [0, 0, 0, 0, 0, 0, 0, 0]
-    noSolution = False;
+    noSolution = False
     MRV_stack = []
     k = 0
-    # if MRV:
-    #     MRV_stack.append(k)
-    LCV_stack = [[],[],[], [],[],[],[],[]]
-
+    LCV_stack = [[], [], [], [], [], [], [], []]
     while k >= 0 and noSolution == False:
-
-        while vector[k] <= 8:
-            print("new Itteration", vector)
-            print("mvr stack", MRV_stack)
-            print("k ", k)
-            print("LCV stack", LCV_stack)
+        savedSteps.append(list)
+        while vector[k] <= 7:
             if LCV:
-                # print("k", k)
                 temp = lcv(k, vector, LCV_stack[k])
                 if temp != 0:
                     vector[k] = temp
                     LCV_stack[k].append(temp)
-                    MRV_stack.append(k)
                 else:
-                    # if MRV_stack:
-                    #     print(k)
-                    #     print(MRV_stack)
-                    print("no modify ", vector)
                     vector[k] = 0
                     LCV_stack[k] = []
-
                     break
-                # print(vector)
             else:
                 vector[k] = vector[k] + 1
-            print("after modify", vector, "\n")
             if is_solution(vector):
-                print(vector)
                 for i in range(0, len(vector)):
                     vector[i] -= 1
-                print(vector)
                 return vector
             elif is_p_and_L(vector):
                 if MRV:
+                    MRV_stack.append(k)
                     k = mrv(vector)
                 else:
                     k += 1
         vector[k] = 0
-
         if MRV:
             if MRV_stack:
                 k = MRV_stack.pop()
-                print("backtracking k= ", k, "\n")
+                print("k with MRV", k)
             else:
                 break
         else:
@@ -940,20 +629,10 @@ def backtraking_with_mrv (MRV,LCV):
 
 
 
-
-
-
-
 def main():
     theme = 'Reddit'
-    # print(backtracking())
-    print(mrv([1,4,7,3,0,0,0,0]))
-    # print(backtraking_with_mrv(True, True))
-    # print(lcv(3,[1,4,7,0,0,0,0,0],[3,5]))
     pecg = EasyChessGui(theme)
-
     pecg.main_loop()
-
 
 
 if __name__ == "__main__":
